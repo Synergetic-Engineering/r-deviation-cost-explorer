c539472b010e325d94fbca8724c147e3be7f9977
Alex Mueller <2556429+lemonpkt@users.noreply.github.com>
2018-01-11 14:53:49 +1000

    Add deviation variable extraction
    Add utility functions which help to extract deviation data for a given
    deviated variable / component. Make plot functions generic so they can
    plot data extracted for any deviated variable.

--- read.R
+++ read.R
@@ -2,48 +2,97 @@ library(tidyverse)
 library(ggplot2)
 library(lubridate)
 
-# TODO: Split into components
+# --------------------- #
+# Read and process data #
+# --------------------- #
 
 # Read data and order by time
 data <- read_csv("data/deviation_data.csv") %>%
   arrange(UnixTime) %>%
   mutate(datetime = as_datetime(UnixTime))
 
+# ------------------------------------------ #
+# Utility functions for extracting from data #
+# ------------------------------------------ #
+
+# Extract deviation variable table from data
 #
-# Example plot for blr.comb.c1in.massFlow.cost
-#
-condenser_example <- function() {
+# Given data, and a deviated variable (as a character), returns a table
+# containing `datetime`, `base_cost`, and `deviated_cost` for that component.
+extract <- function(data, variable) {
+  stopifnot(is.data.frame(data))
+  stopifnot(is.character(variable))
+  stopifnot(length(variable) == 1)
+  
+  base_cost_col <- "corp=ambergilbertenergy,site=ambergilbert,unit=unit1,module=deviation/unit,step=base,io=output,measure=blr.comb.c1in.massFlow.cost"
+  
+  # Find deviated cost column for given variable
+  var_col_regex <- str_c("deviated_variable=", variable, ",measure=blr\\.comb\\.c1in\\.massFlow\\.cost$")
+  idx <- str_which(colnames(data), var_col_regex)
+  if (length(idx) == 0) {
+    stop(str_c("No such variable found \"", variable, "\""))
+  }
+  if (length(idx) > 1) {
+    stop(str_c("Multiple matches found for \"", variable, "\": ", str_c(idx, collapse = ", ")))
+  }
+  deviated_cost_col <- colnames(data)[idx]
+  
   data %>%
-    prep_condenser() %>%
-    # plot_deviated_baseline()
-    # plot_aggregated_dev_cost()
-    # plot_bars()
-    plot_bars_cmp()
+    select(datetime, base_cost = !!base_cost_col, deviated_cost = !!deviated_cost_col) %>%
+    return()
 }
 
-# Extract base, deviated, and deviation costs for condenser from data
-prep_condenser <- function(data) {
-  condenser_data <- data %>%
-    rename(
-      base_cost = `corp=ambergilbertenergy,site=ambergilbert,unit=unit1,module=deviation/unit,step=base,io=output,measure=blr.comb.c1in.massFlow.cost`,
-      condenser_deviated_cost = `corp=ambergilbertenergy,site=ambergilbert,unit=unit1,module=deviation/unit,step=deviation,io=output,deviated_variable=turb.cond.thermalConductance.target,measure=blr.comb.c1in.massFlow.cost`,
-      generator_load = `corp=ambergilbertenergy,site=ambergilbert,unit=unit1,module=deviation/unit,step=base,io=input,measure=turb.gen.c2out.energyFlow.use`
-    ) %>%
-    mutate(
-      deviation_cost_balanced = (base_cost - condenser_deviated_cost) / generator_load,
-      deviation_cost = base_cost - condenser_deviated_cost,
-      agg_dev_cost = cumsum(deviation_cost)
-    )
-  return(condenser_data)
+# Compute deviation cost for data
+#
+# Expects table with `base_cost` and `deviated_cost`.
+# Returns new table with `deviation_cost`.
+process_dev <- function(variable_data) {
+  stopifnot(is.data.frame(variable_data))
+  stopifnot("base_cost" %in% colnames(variable_data))
+  stopifnot(is.numeric(variable_data$base_cost))
+  stopifnot("deviated_cost" %in% colnames(variable_data))
+  stopifnot(is.numeric(variable_data$deviated_cost))
+  
+  variable_data %>%
+    mutate(deviation_cost = base_cost - deviated_cost) %>%
+    return()
+}
+
+# Validate processed deviated variable data
+#
+# Throw an error if table does not contain `base_cost`, `deviated_cost`,
+# `deviation_cost`, or `datetime`. Returns NULL.
+validate <- function(variable_data) {
+  stopifnot(is.data.frame(variable_data))
+  
+  stopifnot("base_cost" %in% colnames(variable_data))
+  stopifnot(is.numeric(variable_data$base_cost))
+  stopifnot("deviated_cost" %in% colnames(variable_data))
+  stopifnot(is.numeric(variable_data$deviated_cost))
+  stopifnot("deviation_cost" %in% colnames(variable_data))
+  stopifnot(is.numeric(variable_data$deviation_cost))
+  stopifnot("datetime" %in% colnames(variable_data))
+  stopifnot(is.timepoint(variable_data$datetime))
+  
+}
+
+# ------------------ #
+# Plotting functions #
+# ------------------ #
+
+condenser_example <- function() {
+  data %>% extract("turb.cond.thermalConductance.target") %>% process_dev() %>% plot_bars_cmp()
 }
 
 # Plot deviated cost and baseline cost together
-plot_deviated_baseline <- function(condenser_data) {
-  scale_factor <- 50.0
-  shift_factor <- mean(prep_condenser(data)$base_cost, na.rm = TRUE)
+plot_deviated_baseline <- function(data) {
+  validate(data)
   
-  ggplot(data = condenser_data) +
-    geom_line(mapping = aes(x = datetime, y = condenser_deviated_cost), colour = "red") +
+  scale_factor <- 50.0
+  shift_factor <- mean(data$base_cost, na.rm = TRUE)
+  
+  ggplot(data = data) +
+    geom_line(mapping = aes(x = datetime, y = deviated_cost), colour = "red") +
     geom_line(mapping = aes(x = datetime, y = base_cost), colour = "black") +
     geom_line(mapping = aes(x = datetime, y = (deviation_cost * scale_factor) + shift_factor), colour = "blue", alpha = 0.5) +
     scale_y_continuous(sec.axis = ~ (. - shift_factor) / scale_factor) +
@@ -51,14 +100,20 @@ plot_deviated_baseline <- function(condenser_data) {
 }
 
 # Plot the aggregated deviation cost
-plot_aggregated_dev_cost <- function(condenser_data) {
-  ggplot(data = condenser_data) +
+plot_aggregated_dev_cost <- function(data) {
+  validate(data)
+  
+  data <- data %>%
+    mutate(agg_dev_cost = cumsum(deviation_cost))
+  ggplot(data = data) +
     geom_line(mapping = aes(x = datetime, y = agg_dev_cost))
 }
 
 # Plot a bar chart of the summative deviation cost for each day
-plot_bars <- function(condenser_data) {
-  bar_data <- condenser_data %>%
+plot_bars <- function(data) {
+  validate(data)
+  
+  bar_data <- data %>%
     group_by(day = as.Date(datetime)) %>%
     summarise(cost = sum(deviation_cost))
   
@@ -69,8 +124,10 @@ plot_bars <- function(condenser_data) {
 }
 
 # Plot a bar chart of the summative deviation cost for each day compared to the average (for each day)
-plot_bars_cmp <- function(condenser_data) {
-  bar_data <- condenser_data %>%
+plot_bars_cmp <- function(data) {
+  validate(data)
+  
+  bar_data <- data %>%
     group_by(day = as.Date(datetime)) %>%
     summarise(cost = sum(deviation_cost, na.rm = TRUE)) %>%
     mutate(cost_diff = mean(cost, na.rm = TRUE) - cost)
